6b280376194a9e0dd872fdd89dfc356629d41d89
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-06-18 22:06:27 +0200

    Desactivación de consulta de posts en página de inicio

--- store/index.js
+++ store/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line import/prefer-default-export
 export const actions = {
   nuxtServerInit({ dispatch, state }) {
-    dispatch('tags/loadAllTags');
-    dispatch('tags/selectTag', state.tags.all.find((tag) => tag.name === null));
+    // dispatch('tags/loadAllTags');
+    // dispatch('tags/selectTag', state.tags.all.find((tag) => tag.name === null));
   },
 };
