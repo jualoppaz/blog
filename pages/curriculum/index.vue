822e5a3da326fd3fe1ba4ee5a157e0425a5a6f79
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-11-02 22:14:22 +0100

    #1 Implementación provisional de la vista de curriculum

---
+++ pages/curriculum/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="curriculum">
+    Aquí va mi Currículum
+  </div>
+</template>
+
+<script>
+export default {
+  beforeDestroy() {
+    this.$store.dispatch('posts/destroyCurrent');
+  },
+};
+</script>
