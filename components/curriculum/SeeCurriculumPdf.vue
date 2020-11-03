0afb5eee02e68a0d4285ae2f1e5f3541650dc5e0
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-11-03 17:57:44 +0100

    #2 Creación de componente para descargar CV en formato PDF

---
+++ components/curriculum/SeeCurriculumPdf.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="curriculum-pdf-format">
+    <div id="text">
+      {{ curriculumPdfFormatText }} Consultar en formato PDF
+      <a
+        href="/docs/Curriculum.pdf"
+        target="_blank"
+        title="xoxVisualizar"
+        class="file-pdf-icon"
+      >
+        <font-awesome-icon
+          :icon="['far', 'file-pdf']"
+          size="lg"
+        />
+      </a>
+      <a
+        href="/docs/Curriculum.pdf"
+        target="_self"
+        download=""
+        title="xoxDescargar"
+        class="download-icon"
+      >
+        <font-awesome-icon
+          :icon="['fas', 'download']"
+          size="lg"
+        />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+#curriculum-pdf-format{
+  > #text {
+    font-size: 24px;
+
+    .file-pdf-icon, .download-icon {
+      color: $color-text-red-dark;
+    }
+  }
+}
+</style>
