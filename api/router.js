37c8f366eddb423113353a8dbf51d5ffb347647a
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-11-05 18:23:46 +0100

    #4 Eliminación de ruta del fichero de verificación de google de la api

--- api/router.js
+++ api/router.js
@@ -9,8 +9,4 @@ router.get('/*', (req, res) => {
   res.status(404).send('not-found');
 });
 
-router.get('/google1e2e247e7cbf40b6.html', (req, res) => {
-  res.status(200).render('google1e2e247e7cbf40b6');
-});
-
 module.exports = router;
