cef38a9b878684474f5574c8ad0d32ef32101bd6
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-11-09 19:54:51 +0100

    #8 Implementación de componente Languages con los idiomas conocidos

---
+++ components/curriculum/Languages.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="languages">
+    <h1 class="curriculum-section-title">
+      <font-awesome-icon
+        :icon="['fas', 'users']"
+        size="1x"
+      />
+      {{ languagesTitle }}
+    </h1>
+    <el-divider />
+    <MediaList
+      :items="languages"
+    >
+      <template v-slot:image="{ item: language }">
+        <img :src="`${language.image}`">
+      </template>
+      <template v-slot:title="{ item: language }">
+        {{ language.level }} {{ language.name }} ({{ language.date }})
+      </template>
+      <template v-slot:body="{ item: language }">
+        {{ language.description }}
+      </template>
+    </MediaList>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import MediaList from '@/components/MediaList.vue';
+
+export default {
+  name: 'Languages',
+  components: {
+    MediaList,
+  },
+  data() {
+    return {
+      languagesTitle: this.$t('VIEWS.CV.LANGUAGES.TITLE'),
+    };
+  },
+  computed: {
+    ...mapState('curriculum', {
+      languages: 'languages',
+    }),
+  },
+};
+</script>
