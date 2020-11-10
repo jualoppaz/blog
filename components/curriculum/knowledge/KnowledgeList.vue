59ba50b88996e482605f0e7cc4901cb2e94fa6db
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-11-10 18:07:46 +0100

    #7 Implementación de vista y componentes para visualizar mis conocimientos en el currículum

---
+++ components/curriculum/knowledge/KnowledgeList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="knowledge-list">
+    <h1 class="knowledge-list-title">
+      <slot name="title" />
+      <el-row :gutter="20">
+        <el-col
+          v-for="item in items"
+          :key="item.id"
+          :sm="{
+            span: 8
+          }"
+        >
+          <KnowledgeItem
+            :item="item"
+          />
+        </el-col>
+      </el-row>
+    </h1>
+  </div>
+</template>
+
+<script>
+import KnowledgeItem from '@/components/curriculum/knowledge/KnowledgeItem.vue';
+
+export default {
+  name: 'KnowledgeList',
+  components: {
+    KnowledgeItem,
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
