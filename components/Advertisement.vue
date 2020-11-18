52afa1aab0bd1837c5a06c1e765e5af9c6751aad
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-11-18 19:19:04 +0100

    Creación de componente para incluir anuncio de Google AdSense

---
+++ components/Advertisement.vue
@@ -0,0 +1,13 @@
+<template>
+  <adsbygoogle
+    data-ad-slot="6492981921"
+    data-ad-format="fluid"
+    data-full-width-responsive="true"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Advertisement',
+};
+</script>
