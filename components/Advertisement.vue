7e4ac965461a485084e65ec1bf4d7f576afb603a
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-11-18 19:28:25 +0100

    Ajustes en la maquetación del componente Advertisement

--- components/Advertisement.vue
+++ components/Advertisement.vue
@@ -1,9 +1,11 @@
 <template>
-  <adsbygoogle
-    data-ad-slot="6492981921"
-    data-ad-format="fluid"
-    data-full-width-responsive="true"
-  />
+  <div class="advertisement">
+    <adsbygoogle
+      data-ad-slot="6492981921"
+      data-ad-format="fluid"
+      data-full-width-responsive="true"
+    />
+  </div>
 </template>
 
 <script>
@@ -11,3 +13,9 @@ export default {
   name: 'Advertisement',
 };
 </script>
+
+<style lang="scss" scoped>
+.advertisement{
+  margin-top: 15px;
+}
+</style>
