4edf60a8a2f3f988423f51ab7cc467f04adccf42
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-12-01 19:19:37 +0100

    #14 Implementación de media por crédito junto con los literales dinamizados y los el-tag para dar colorido a la tabla

--- components/academic-record/averages/AcademicCourseAverages.vue
+++ components/academic-record/averages/AcademicCourseAverages.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="academic-course-averages">
     <h1 id="academic-course-averages-title">
-      xoxMedias por Curso Académico
+      {{ headerTitle }}
     </h1>
     <el-divider />
     <el-table
@@ -9,15 +9,36 @@
       border
     >
       <el-table-column
-        label="xoxCurso académico"
+        :label="academicCourseLabel"
         prop="academicCourse"
         align="center"
       />
       <el-table-column
-        label="xoxMedia por Asignatura"
-        prop="subjectAverage"
+        :label="subjectAverageLabel"
         align="center"
-      />
+      >
+        <template slot-scope="props">
+          <el-tag
+            :type="getMarkType(props.row.subjectAverage)"
+            effect="dark"
+          >
+            {{ props.row.subjectAverage }}
+          </el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column
+        :label="creditAverageLabel"
+        align="center"
+      >
+        <template slot-scope="props">
+          <el-tag
+            :type="getMarkType(props.row.creditAverage)"
+            effect="dark"
+          >
+            {{ props.row.creditAverage }}
+          </el-tag>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -25,6 +46,14 @@
 <script>
 export default {
   name: 'AcademicCourseAverages',
+  data() {
+    return {
+      headerTitle: this.$t('VIEWS.ACADEMIC_RECORD.ACADEMIC_COURSES.ACADEMIC_COURSE_AVERAGES_TABLE.HEADER.TITLE'),
+      academicCourseLabel: this.$t('VIEWS.ACADEMIC_RECORD.ACADEMIC_COURSES.ACADEMIC_COURSE_AVERAGES_TABLE.ACADEMIC_COURSE.LABEL'),
+      subjectAverageLabel: this.$t('VIEWS.ACADEMIC_RECORD.ACADEMIC_COURSES.ACADEMIC_COURSE_AVERAGES_TABLE.SUBJECT_AVERAGE.LABEL'),
+      creditAverageLabel: this.$t('VIEWS.ACADEMIC_RECORD.ACADEMIC_COURSES.ACADEMIC_COURSE_AVERAGES_TABLE.CREDIT_AVERAGE.LABEL'),
+    };
+  },
   computed: {
     subjectAverageByAcademicCourse20102011() {
       return this.$store.getters['academic-records/getSubjectAverageByAcademicCourse']('2010/2011');
@@ -38,22 +67,47 @@ export default {
     subjectAverageByAcademicCourse20132014() {
       return this.$store.getters['academic-records/getSubjectAverageByAcademicCourse']('2013/2014');
     },
+    creditAverageByAcademicCourse20102011() {
+      return this.$store.getters['academic-records/getCreditAverageByAcademicCourse']('2010/2011');
+    },
+    creditAverageByAcademicCourse20112012() {
+      return this.$store.getters['academic-records/getCreditAverageByAcademicCourse']('2011/2012');
+    },
+    creditAverageByAcademicCourse20122013() {
+      return this.$store.getters['academic-records/getCreditAverageByAcademicCourse']('2012/2013');
+    },
+    creditAverageByAcademicCourse20132014() {
+      return this.$store.getters['academic-records/getCreditAverageByAcademicCourse']('2013/2014');
+    },
     rows() {
       return [
         {
           academicCourse: '2010/2011',
           subjectAverage: this.subjectAverageByAcademicCourse20102011,
+          creditAverage: this.creditAverageByAcademicCourse20102011,
         }, {
           academicCourse: '2011/2012',
           subjectAverage: this.subjectAverageByAcademicCourse20112012,
+          creditAverage: this.creditAverageByAcademicCourse20112012,
         }, {
           academicCourse: '2012/2013',
           subjectAverage: this.subjectAverageByAcademicCourse20122013,
+          creditAverage: this.creditAverageByAcademicCourse20122013,
         }, {
           academicCourse: '2013/2014',
           subjectAverage: this.subjectAverageByAcademicCourse20132014,
+          creditAverage: this.creditAverageByAcademicCourse20132014,
         },
       ];
+    },
+  },
+  methods: {
+    getMarkType(average) {
+      if (average >= 9) return 'warning';
+      if (average >= 7) return '';
+      if (average >= 5) return 'success';
+      if (average < 5) return 'fail';
+      return 'info';
     },
   },
 };
